2dd7c23f9402d9cc6fa0d60ab2e30e200107939c
Kirill <befordshir@gmail.com>
2025-02-23 17:06:51 +0300

    scale fixes, + 1 page

--- main_page/urls.py
+++ main_page/urls.py
@@ -7,6 +7,7 @@ app_name = 'main_page'
 urlpatterns = [
     path('', views.mainPage, name = 'main_page'),
     path('temp/', views.mainPage2, name = 'main_page2'),
+    path('flooding/', views.flooding, name = 'flooding'),
     path('css/styles.css', TemplateView.as_view(
         template_name='styles.css',
         content_type='text/css')
--- main_page/views.py
+++ main_page/views.py
@@ -7,7 +7,7 @@ from .forms import ContactForm
 
 def mainPage(request):
     if request.method == 'POST':
-        form =  ContactForm(request.POST)
+        form = ContactForm(request.POST)
         if form.is_valid():
             name = form.cleaned_data['name']
             phone = form.cleaned_data['phone']
@@ -26,9 +26,34 @@ def mainPage(request):
     else:
         form = ContactForm()
 
-    return render(request, 'main_page/bootstrap.html', {'form':form})
+    return render(request, 'main_page/main_page.html', {'form':form})
 
 
 def mainPage2(request):
     return render(request, 'main_page/index.html')
+
+def flooding(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            phone = form.cleaned_data['phone']
+            comment = form.cleaned_data['comment']
+            name_phone = f'{name} {phone}'
+
+            recipients = ['befordshir@gmail.com']
+
+            try:
+                send_mail(name_phone, comment, 'sunbaking@yandex.ru', recipients)
+
+            except BadHeaderError:
+                return HttpRequest('Invalid header found')
+            
+            return render(request, 'main_page/index.html', {'form': form})
+        
+    else:
+        form = ContactForm()
+
+    return render(request, 'main_page/flooding.html', {'form': form})
+
 
