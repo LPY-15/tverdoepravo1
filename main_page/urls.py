a9786aa3e7edde44d57a21f169bc583b9de34427
Kirill <befordshir@gmail.com>
2025-03-08 01:33:14 +0300

    almost done

--- main_page/urls.py
+++ main_page/urls.py
@@ -10,6 +10,12 @@ urlpatterns = [
     path('flooding/', views.flooding, name = 'flooding'),
     path('expertise/', views.expertise, name = 'expertise'),
     path('example/', views.example, name = 'example'),
+    path('pre-trial/', views.pre_trial, name = 'pre-trial'),
+    path('making-documents/', views.making_documents, name = 'making_documents'),
+    path('labor-disputes/', views.labor_disputes, name = 'labor_disputes'),
+    path('cancellation/', views.cancellation, name = 'cancellation'),
+    path('refusal/', views.refusal, name = 'refusal'),
+    path('trademark/', views.trademark, name = 'trademark'),
     path('css/styles.css', TemplateView.as_view(
         template_name='styles.css',
         content_type='text/css')
--- main_page/views.py
+++ main_page/views.py
@@ -116,3 +116,157 @@ def example(request):
 
 
 
+def pre_trial(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            phone = form.cleaned_data['phone']
+            comment = form.cleaned_data['comment']
+            name_phone = f'{name} {phone}'
+
+            recipients = ['befordshir@gmail.com']
+
+            try:
+                send_mail(name_phone, comment, 'sunbaking@yandex.ru', recipients)
+
+            except BadHeaderError:
+                return HttpRequest('Invalid header found')
+            
+        return render(request, 'main_page/pre-trial.html', {'form': form})
+    
+    else:
+        form = ContactForm()
+
+    return render(request, 'main_page/pre-trial.html', {'form': form})
+
+def making_documents(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            phone = form.cleaned_data['phone']
+            comment = form.cleaned_data['comment']
+            name_phone = f'{name} {phone}'
+
+            recipients = ['befordshir@gmail.com']
+
+            try:
+                send_mail(name_phone, comment, 'sunbaking@yandex.ru', recipients)
+
+            except BadHeaderError:
+                return HttpRequest('Invalid header found')
+            
+        return render(request, 'main_page/making_documents.html', {'form': form})
+    
+    else:
+        form = ContactForm()
+    
+    return render(request, 'main_page/making_documents.html', {'form': form})
+
+
+
+def labor_disputes(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            phone = form.cleaned_data['phone']
+            comment = form.cleaned_data['comment']
+
+            name_phone = f'{name} {phone}'
+
+            recipients = ['befordshir@gmail.com']
+
+            try:
+                send_mail(name_phone, comment, 'sunbaking@yandex.ru', recipients)
+
+            except BadHeaderError:
+                return HttpRequest('Invalid header found')
+            
+        return render(request, 'main_page/labor_disputes.html', {'form': form})
+    
+    else:
+        form = ContactForm()
+
+    return render(request, 'main_page/labor_disputes.html', {'form': form})
+
+def cancellation(request):
+    if request.method =='POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            phone = form.cleaned_data['phone']
+            comment = form.cleaned_data['comment']
+
+            name_phone = f'{name} {phone}'
+
+            recipients = ['befordshir@gmail.com']
+
+            try:
+                send_mail(name_phone, comment, 'sunbaking@yandex.ru', recipients)
+
+            except BadHeaderError:
+                return HttpRequest('Invalid header found')
+            
+        return render(request, 'main_page/cancellation.html', {'form': form})
+    
+    else:
+        form = ContactForm()
+
+    return render(request, 'main_page/cancellation.html', {'form': form})
+
+
+
+def refusal(request):
+    if request.method =='POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            phone = form.cleaned_data['phone']
+            comment = form.cleaned_data['comment']
+
+            name_phone = f'{name} {phone}'
+
+            recipients = ['befordshir@gmail.com']
+
+            try:
+                send_mail(name_phone, comment, 'sunbaking@yandex.ru', recipients)
+
+            except BadHeaderError:
+                return HttpRequest('Invalid header found')
+            
+        return render(request, 'main_page/refusal.html', {'form': form})
+    
+    else:
+        form = ContactForm()
+
+    return render(request, 'main_page/refusal.html', {'form': form})
+
+
+
+def trademark(request):
+    if request.method =='POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            phone = form.cleaned_data['phone']
+            comment = form.cleaned_data['comment']
+
+            name_phone = f'{name} {phone}'
+
+            recipients = ['befordshir@gmail.com']
+
+            try:
+                send_mail(name_phone, comment, 'sunbaking@yandex.ru', recipients)
+
+            except BadHeaderError:
+                return HttpRequest('Invalid header found')
+            
+        return render(request, 'main_page/trademark.html', {'form': form})
+    
+    else:
+        form = ContactForm()
+
+    return render(request, 'main_page/trademark.html', {'form': form})
+
