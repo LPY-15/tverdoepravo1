4329c3f177eecb1f73516b7544f14f182541395d
Kirill <befordshir@gmail.com>
2025-04-05 17:52:20 +0300

    123

--- tverdoepravo/settings.py
+++ tverdoepravo/settings.py
@@ -136,4 +136,4 @@ EMAIL_HOST_USER = 'befordshir@mail.ru' #Например, user@gmail.com. Име
 EMAIL_HOST_PASSWORD = 'axfYCFFfxaw7ctZt6m1s'
 #'dulviftmzkssindd -yandex' jjfnvdnoczydmdsm
 
-CSRF_TRUSTED_ORIGINS=['https://lpy-15.ru']
+CSRF_TRUSTED_ORIGINS=['https://lpy-15.ru', 'https://tverdoepravo.site']
